0f47c7d1a73e4e5b6b30f49edf42d57ba3c74fe5
Julian <jb@futureplay.de>
2017-09-21 21:12:09 +0200

    removed not used utils; added a new function

--- Ooyodo/utils/utils.py
+++ Ooyodo/utils/utils.py
@@ -7,17 +7,9 @@ import os
 from time import gmtime, time, sleep
                     
 
-async def delete_last_message(client, message, string=""):
-    async for mes in client.logs_from(message.channel):
-        if mes.content.startswith("<@"+message.author.id+">") and mes.author == client.user:
-            await client.delete_message(mes)
-            if len(string) > 0:
-                await client.send_message(message.channel, string)
-            break
-
-def check(string):
-    try:
-        int(string)
-        return True
-    except:
-        return False
+async def get_channel_by_id(client, id):
+    for s in client.servers:
+        for c in s.channels:
+            if c.id == id:
+                return c
+    return None
