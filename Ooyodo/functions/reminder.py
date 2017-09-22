9e4d497e2da1c19bf4e5a27785059075341e1fa0
Julian <jb@futureplay.de>
2017-09-22 13:23:52 +0200

    added more comments and cleaned the code a bit

--- Ooyodo/functions/reminder.py
+++ Ooyodo/functions/reminder.py
@@ -12,15 +12,15 @@ It takes the client as an argument to be able to send messages
 """
 async def pvp(client):
     first = (17 - gmtime(time()).tm_hour) % 12
-    kancolle_stuff = '242858060224135168'
-    channel = await get_channel_by_id(client, kancolle_stuff)
+    kancolle_stuff_id = '242858060224135168'
+    channel = await get_channel_by_id(client, kancolle_stuff_id)
 
     if channel is None:
         print('Couldn\'t find kancolle stuff!')
         return
 
     # Debug Print
-    print("I should remind in:", first, 'hours and', 30 - gmtime(time()).tm_min % 60, 'minutes in', channel.name)
+    print("I should remind in:", first, 'hours and', 30 - gmtime(time()).tm_min % 60, 'minutes in', channel.name, 'for pvp.')
 
 
 
@@ -31,16 +31,23 @@ async def pvp(client):
         #print('Sleeping for another', (17 - gmtime(time()).tm_hour) % 12, 'hours and', (30 - gmtime(time()).tm_min) % 60, 'minutes')
         await asyncio.sleep(60)
 
+    first = 12
 
     while True:
-        #prepare and send the 30 minute before warning
-        embed = discord.Embed(description=":exclamation: PvP will reset in about 30 minutes", color=discord.Colour(0xba7c6b))
+        # prevent timedrift due to different clocks
+        while gmtime(time()).tm_min != 30:
+            await asyncio.sleep(10)
+
+        # prepare and send the 30 minute before warning
+        embed = discord.Embed(description=":exclamation: PvP will reset in about 30 minutes", color=discord.Colour(0xb736b0))
         await client.send_message(channel, embed=embed)
 
+        # wait the 30 minutes 
         await asyncio.sleep(30*60)
 
+        # prepare and send the reset message
         embed = discord.Embed(description=":exclamation: PvP is reseted", color=discord.Colour(0xff0000))
         await client.send_message(channel, embed=embed)
 
-        first = 12
+        # wait 12 hours minus 30 minutes for the warning
         await asyncio.sleep((first*60*60)-30*60)
