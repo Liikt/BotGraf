9f9bc2c2264f41b743976977533bcdc706359a7f
Arlios <luyen_mai@Live.com>
2017-09-24 07:42:00 -0700

    Tiny changes to output text

--- Ooyodo/functions/reminder.py
+++ Ooyodo/functions/reminder.py
@@ -132,11 +132,11 @@ async def quests(client):
                 remaining = 'or two days'
 
             # Send the message for the week or day before warning
-            desc = ":exclamation: {} Admiral, quests will reset in about one {}".format(desc, remaining)
+            desc = ":exclamation:  Admiral, {} quests will reset in about one {}".format(desc, remaining)
             embed = discord.Embed(description=desc, color=discord.Colour(0xb736b0))
 
         # prepare and send the 30 minute before warning
-        desc = ":exclamation: {} Admiral, quests will reset in about 30 minutes".format(quests)
+        desc = ":exclamation:  Admiral, {} quests will reset in about 30 minutes".format(quests)
         embed = discord.Embed(description=desc, color=discord.Colour(0xb736b0))
         await client.send_message(channel, embed=embed)
 
@@ -144,7 +144,7 @@ async def quests(client):
         await asyncio.sleep(30*60)
 
         # prepare and send the reset message
-        desc = ":exclamation: {} Admiral, quests have reset".format(quests)
+        desc = ":exclamation:  Admiral, {} quests have reset".format(quests)
         embed = discord.Embed(description=desc, color=discord.Colour(0xff0000))
         await client.send_message(channel, embed=embed)
 
