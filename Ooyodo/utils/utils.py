8d27ccb705eec5abdb9c3defc05191fe56a6cb35
Julian <jb@futureplay.de>
2017-10-01 15:09:35 +0200

    added a function that retrieves multiple channels

--- Ooyodo/utils/utils.py
+++ Ooyodo/utils/utils.py
@@ -51,7 +51,7 @@ channel with the given id.
 
 It takes the client object and the id for the channel to search for
 
-It returns either the channelobject or None if no channel was found
+It returns either the channelobject and the name or None if no channel was found
 """
 async def get_channel_by_id(client, id):
 
@@ -63,7 +63,30 @@ async def get_channel_by_id(client, id):
 
             # Find the channel with the given id
             if c.id == id:
-                return c
+                return s.name, c
 
     # Return None if no channel was found with that id
-    return None
+    return None, None
+
+"""
+get_channel_by_ids will search through every channel in every server to find the
+channel with the given id.
+
+It takes the client object and a list of channelids to search for
+
+It returns the list consiting of tuples of the name, the channelobject and the id of all found channels
+"""
+async def get_channel_by_ids(client, ids):
+    # Initialize a new return list
+    ret = []
+    # Go through all ids
+    for id in ids:
+        # Call get_channel_by_id to get the channel and the name
+        name, channel = get_channel_by_id(client, id)
+        # Check if the channel was found
+        if channel is not None:
+            #If the channel was found append it with all the other information to the list
+            ret.append((name, channel, id))
+
+    # Return the return list
+    return ret
