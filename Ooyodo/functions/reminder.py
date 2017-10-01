86d527eb9e5aa3f071eab8fed89623e500a05095
Julian <jb@futureplay.de>
2017-10-01 15:09:59 +0200

    added support for multiple channels

--- Ooyodo/functions/reminder.py
+++ Ooyodo/functions/reminder.py
@@ -17,18 +17,20 @@ async def pvp(client):
     # Calculate the time difference and when to send the first message
     first = (17 - gmtime(time()).tm_hour) % 12
 
-    # Find the channel where to send the reminder
-    channel_id = '242858060224135168'
-    channel = await get_channel_by_id(client, channel_id)
+    # Find the channels where to send the reminder
+    channel_ids = ['242858060224135168']
+    channels = await get_channel_by_ids(client, channel_ids)
 
-    # Check if the channel was found
-    if channel is None:
-        log('WARN', 'pvp', "Couldn't channel with id {}".format(channel_id))
-        return
+    # Check if the all the channels were found
+    if len(channels) != len(channel_ids):
+        for channelname, _, id in channels:
+            if id not in channel_ids:
+                log('WARN', 'pvp', "Couldn't find channel with channelname: and id: {}"\
+                    .format(channelname, id))
 
     # Debug log
-    log('INFO', 'pvp', 'I should remind in: {} hours and {} minutes in {} for pvp.'\
-            .format(first, (30-gmtime(time()).tm_min) % 60, channel.name))
+    log('INFO', 'pvp', 'I should remind in: {} hours and {} minutes for pvp.'\
+            .format(first, (30-gmtime(time()).tm_min) % 60))
 
     # Wait a bit to get approximatly the right time to send
     while not 28 < gmtime(time()).tm_min < 31 or (17 - gmtime(time()).tm_hour) % 12 != 0:
@@ -41,19 +43,21 @@ async def pvp(client):
             await asyncio.sleep(10)
 
         # prepare and send the 30 minute before warning
-        desc = ":exclamation: Admiral, PvP's will reset in about 30 minutes"
-        embed = discord.Embed(description=desc, color=discord.Colour(0xffff00))
+        for _, channel, _ in channels:
+            desc = ":exclamation: Admiral, PvP's will reset in about 30 minutes"
+            embed = discord.Embed(description=desc, color=discord.Colour(0xffff00))
 
-        await client.send_message(channel, embed=embed)
+            await client.send_message(channel, embed=embed)
 
         # wait the 30 minutes
         await asyncio.sleep(30*60)
 
         # prepare and send the reset message
-        desc = ":exclamation: Admiral, PvP's have reset."
-        embed = discord.Embed(description=desc, color=discord.Colour(0x00ff00))
-        await client.send_message(channel, embed=embed)
-        log('INFO', 'pvp', 'Sending reminder for pvp')
+        for channelname, channel, _ in channels:
+            desc = ":exclamation: Admiral, PvP's have reset."
+            embed = discord.Embed(description=desc, color=discord.Colour(0x00ff00))
+            await client.send_message(channel, embed=embed)
+            log('INFO', 'pvp', 'Sending reminder for pvp in {}'.format(channelname))
 
         # wait 12 hours minus 30 minutes for the warning
         await asyncio.sleep((12*60*60) - 30*60)
@@ -70,18 +74,20 @@ async def quests(client):
     # Calculate the time difference and when to send the first message
     first = (5 + gmtime(time()).tm_hour) % 24
 
-    # Find the channel where to send the reminder
-    channel_id = '242858060224135168'
-    channel = await get_channel_by_id(client, channel_id)
+    # Find the channels where to send the reminder
+    channel_ids = ['242858060224135168']
+    channels = await get_channel_by_ids(client, channel_ids)
 
-    # Check if the channel was found
-    if channel is None:
-        log('WARN', 'quests', "Couldn't channel with id {}".format(channel_id))
-        return
+    # Check if the all the channels were found
+    if len(channels) != len(channel_ids):
+        for channelname, _, id in channels:
+            if id not in channel_ids:
+                log('WARN', 'quests', "Couldn't find channel with channelname: and id: {}"\
+                    .format(channelname, id))
 
     # Debug log
-    log('INFO', 'quests', 'I should remind in: {} hours and {} minutes in {} for quests.'\
-            .format(first, (30-gmtime(time()).tm_min) % 60, channel.name))
+    log('INFO', 'quests', 'I should remind in: {} hours and {} minutes for quests.'\
+            .format(first, (30-gmtime(time()).tm_min) % 60))
 
     # Wait a bit to get approximatly the right time to send
     while not 28 < gmtime(time()).tm_min < 32 or (5 + gmtime(time()).tm_hour) % 24 != 0:
@@ -116,13 +122,13 @@ async def quests(client):
         # Check if it's the last day of the month or a week before
         if gmtime(time()).tm_mday in [23, 29]:
             # Set the placeholders
-            desc = 'monthly'
+            temp = 'monthly'
             remaining = 'week'
 
             # Check if the quarterlys also are resetting soon
             if gmtime(time()).tm_mon in [2, 5, 8, 11]:
                 # Add quarterlies to the placeholder
-                desc = 'quarterly and ' + desc
+                temp = 'quarterly and ' + temp
 
             # Check if it's actually the last day of the month
             if gmtime(time()).tm_mday == 29:
@@ -130,24 +136,28 @@ async def quests(client):
                 remaining = 'or two days'
 
             # Send the message for the week or day before warning
-            desc = ":exclamation:  Admiral, {} quests will reset in about one {}".format(desc, remaining)
-            embed = discord.Embed(description=desc, color=discord.Colour(0xffff00))
-            await client.send_message(channel, embed=embed)
-            log('INFO', 'quests', 'Sending reminder for {} in one {}'.format(desc, remaining))
+            for channelname, channel, _ in channels:
+                desc = ":exclamation:  Admiral, {} quests will reset in about one {}".format(temp, remaining)
+                embed = discord.Embed(description=desc, color=discord.Colour(0xffff00))
+                await client.send_message(channel, embed=embed)
+                log('INFO', 'quests', 'Sending reminder for {} in one {} in {}'\
+                    .format(temp, remaining, channelname))
 
         # prepare and send the 30 minute before warning
-        desc = ":exclamation:  Admiral, {} quests will reset in about 30 minutes".format(quests)
-        embed = discord.Embed(description=desc, color=discord.Colour(0xffff00))
-        await client.send_message(channel, embed=embed)
+        for _, channel, _ in channels:
+            desc = ":exclamation:  Admiral, {} quests will reset in about 30 minutes".format(quests)
+            embed = discord.Embed(description=desc, color=discord.Colour(0xffff00))
+            await client.send_message(channel, embed=embed)
 
         # wait the 30 minutes
         await asyncio.sleep(30*60)
 
         # prepare and send the reset message
-        desc = ":exclamation:  Admiral, {} quests have reset".format(quests)
-        embed = discord.Embed(description=desc, color=discord.Colour(0x00ff00))
-        await client.send_message(channel, embed=embed)
-        log('INFO', 'quests', 'Sending reminder for {} quests'.format(quests))
+        for channelname, channel, _ in channels:
+            desc = ":exclamation:  Admiral, {} quests have reset".format(quests)
+            embed = discord.Embed(description=desc, color=discord.Colour(0x00ff00))
+            await client.send_message(channel, embed=embed)
+            log('INFO', 'quests', 'Sending reminder for {} quests in {}'.format(quests, channelname))
 
 
         # wait 24 hours minus 30 minutes for the warning
